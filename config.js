4978672ae7483543f039301690c21b111ef011a9
shubhamvocals <20je0931@me.iitism.ac.in>
2023-08-05 19:07:07 +0530

    second commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     CLIENT_ID: 'c28c645da3e3420690ce0a13a1dfc527',
     CLIENT_SECRET: '3634c15e89054e8e88ff8c8f916df4f5',
-    REDIRECT_URI: 'https://cpcodjjmjhibpajdlgjiieifgpmfphoh.chromiumapp.org'
+    REDIRECT_URI: 'https://iedknlaailkplppjooncdgjmlniaadhk.chromiumapp.org'
 };
 
 export default CONFIG;
